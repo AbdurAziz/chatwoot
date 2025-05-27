b090cbcaa745f0b3343208913e34de96682c09ca
Abdur Aziz <80990029+AbdurAziz@users.noreply.github.com>
2025-05-27 18:15:53 -0500

    Update processor_service.rb
    This PR replaces all references of Captain with AI Agent in the Chatwoot codebase to reflect branding changes. Updates include Vue components, localization files, and documentation.

--- lib/integrations/captain/processor_service.rb
+++ lib/integrations/captain/processor_service.rb
@@ -1,10 +1,10 @@
-class Integrations::Captain::ProcessorService < Integrations::BotProcessorService
+class Integrations::ai_agent::ProcessorService < Integrations::BotProcessorService
   pattr_initialize [:event_name!, :hook!, :event_data!]
 
   private
 
   def get_response(_session_id, message_content)
-    call_captain(message_content)
+    call_ai_agent(message_content)
   end
 
   def process_response(message, response)
@@ -30,8 +30,8 @@ class Integrations::Captain::ProcessorService < Integrations::BotProcessorServic
     )
   end
 
-  def call_captain(message_content)
-    url = "#{GlobalConfigService.load('CAPTAIN_API_URL',
+  def call_ai_agent(message_content)
+    url = "#{GlobalConfigService.load('AIAGENT_API_URL',
                                       '')}/accounts/#{hook.settings['account_id']}/assistants/#{hook.settings['assistant_id']}/chat"
 
     headers = {
